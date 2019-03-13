c715dc19a3a0b972cf7a4fa037a3ff5ee22f462d
Pawel Mroz <paulmroz97@gmail.com>
2019-03-13 20:06:46 +0100

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/TrieTree_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrieTree_Project.dir/main.c.o"
+  "TrieTree_Project.pdb"
+  "TrieTree_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TrieTree_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TrieTree_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TrieTree_Project.
+# This may be replaced when dependencies are built.
