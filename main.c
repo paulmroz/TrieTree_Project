276441fd7eb8ab755d0416f5f155d6b986cbb865
Pawel Mroz <paulmroz97@gmail.com>
2019-03-15 22:46:09 +0100

    Added function to create new Node

--- main.c
+++ main.c
@@ -7,18 +7,28 @@
 //Drzewo oparte jest na 26 literowym alfabecie
 #define CHAR_SIZE 26
 
-struct Node {
+struct TrieTree {
 
-    int isLeaf;
-    struct Node *character[CHAR_SIZE];
+    int isLeaf;// 1 jezeli wezel jest lisciem
+    struct TrieTree *children[CHAR_SIZE];
 };
 
+struct TrieTree *getNewTrieNode()
+{
+    //Dynamiczne alokowanie pamieci dla nowego wezla
+    struct TrieTree *newNode = (struct TrieTree *)malloc(sizeof(struct TrieTree));
+    if(newNode){
+        //Ustawianie wartosci isLeafe wezla na 0 oraz wypelnianie tablicy wartosciami NULL
+        newNode->isLeaf = 0;
+        for (int i = 0; i < CHAR_SIZE; i++) {
+            newNode->children[i] = NULL;
+        }
+    }
+    //zwracanie wezla
+    return  newNode;
+}
 
-
-
-
-
-char toUppperCase(char word[]) {
+char toUpperCase(char word[]) {
     int i;
     for(i=0;i<=strlen(word);i++){
         if(word[i]>=97 && word[i]<=122)
@@ -44,18 +54,18 @@ int main() {
             case 1:
                 printf("Wpisz slowo ktore ma być wpisane do drzewa: ");
                 scanf("%s",word);
-                toUppperCase(word);
+                toUpperCase(word);
 
                 break;
             case 2:
                 printf("Wpisz slowo ktore ma byc sprwadzone czy istnieje w drzewie: ");
                 scanf("%s",word);
-                toUppperCase(word);
+                toUpperCase(word);
                 break;
             case 3:
                 printf("Wpisz slowo ktore ma byc usuniete z drzewa: ");
                 scanf("%s",word);
-                toUppperCase(word);
+                toUpperCase(word);
                 break;
             case 4: break;
             default:
