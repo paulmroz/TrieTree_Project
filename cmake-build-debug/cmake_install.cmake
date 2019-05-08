92b773c719bbc0939378b560b437e13684fbb085
Pawel Mroz <paulmroz97@gmail.com>
2019-05-08 18:57:08 +0200

    Working on delete function

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/paulm/Desktop/Pulpit/Projects/TrieTree_Project
+# Install script for directory: /home/paulm/Desktop/Desktop/Projects/TrieTree_Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/paulm/Desktop/Pulpit/Projects/TrieTree_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/paulm/Desktop/Desktop/Projects/TrieTree_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
